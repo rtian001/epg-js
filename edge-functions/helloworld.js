26dfa683910edb009330b6786191640c21100455
rtian001 <rtian001@gmail.com>
2026-03-17 09:21:21 +0800

    modified:   edge-functions/helloworld.js

--- edge-functions/helloworld.js
+++ edge-functions/helloworld.js
@@ -1,7 +1,7 @@
 // 文件路径 ./edge-functions/api/hello.js
 // 访问路径 example.com/api/hello
 export default function onRequest(context) {
-  return new Response({ 'message': 'Hello from Epg!' }, {
+  return new Response(JSON.stringify({ 'message': 'Hello from Epg!' }), {
     headers: {
       'Content-Type': 'application/json',
     },
