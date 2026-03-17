20ffdc9a2acfb5431b6a0363a605784e43ac8724
rtian001 <rtian001@gmail.com>
2026-03-17 09:54:55 +0800

    modified:   edge-functions/index.js

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -3,16 +3,16 @@ export default async function onRequest(context) {
   const params = new URLSearchParams(query);
   const channel = params.get('ch') || 'cctv1';
   const currentDate = new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toISOString().slice(0, 10).replace(/-/g, '');
-
   const date = params.get('date') || currentDate;
-  const eJson = context.waitUntil(fetch('./e.json'));
-  const eJsonData = await eJson.json();
+  var epgData = [];
+  // const eJson = context.waitUntil(fetch('./e.json'));
+  // const eJsonData = await eJson.json();
   // const epgData = eJsonData[channel][date];
-  // const data = {
-  //   'channel_name': channel,
-  //   'date': date,
-  //   'epg_data': epgData,
-  // };
+  const data = {
+    'channel_name': channel,
+    'date': date,
+    'epg_data': epgData,
+  };
   return new Response(JSON.stringify(eJsonData), {
     headers: {
       'Content-Type': 'application/json',
