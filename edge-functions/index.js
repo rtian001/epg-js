5e7cae5b07fe5ceb928c3c0db93af92a0d14278e
rtian001 <rtian001@gmail.com>
2026-03-17 09:26:14 +0800

    modified:   edge-functions/index.js

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -1,5 +1,12 @@
-// 文件路径 ./edge-functions/api/hello.js
-// 访问路径 example.com/api/hello
 export default function onRequest(context) {
-  return new Response('Hello from Edge Functions!');
+  const data = {
+    success: true,
+    message: 'Hello from Epg!',
+    channel: 'CCTV1'
+  };
+  return new Response(JSON.stringify(data), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
