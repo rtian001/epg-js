fb831494d6af863988954e2e6f50bfb11c3852fd
rtian001 <rtian001@gmail.com>
2026-03-17 08:37:07 +0800

    update

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -1,5 +1,5 @@
 // 文件路径 ./edge-functions/api/hello.js
 // 访问路径 example.com/api/hello
 export default function onRequest(context) {
-  return new Response('Hello from Edge Functions!');
+  return Response.json({ "message": 'Hello from Edge Functions!' });
 }
