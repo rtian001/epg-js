1e42a453ce432fcebab2748e48c434ff58b420fb
rtian001 <rtian001@gmail.com>
2026-03-18 12:26:14 +0800

    modified:   edge-functions/api/epg.js

--- edge-functions/api/epg.js
+++ edge-functions/api/epg.js
@@ -1,10 +1,32 @@
-export default function onRequest(context) {
-    const data = {
-        success: true,
-        message: 'Hello from Epg!',
-        channel: 'CCTV1'
+export default async function onRequest(context) {
+    const req = context.request;
+    const url = new URL(req.url);
+    const search = url.search;
+    // const query = context.request.url.split('?')[1];
+    const params = new URLSearchParams(search);
+    const channel = params.get('ch') || 'cctv1';
+    const currentDate = new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toISOString().slice(0, 10);
+    const date = params.get('date') || currentDate;
+    const host = req.headers.get('host');
+    var epgData = [{
+        start: '00:00',
+        end: '23:59',
+        title: '精彩节目'
+    }];
+    const eJson = await fetch(`https://${host}/epg-${date}.json`);
+    const eJsonData = await eJson.json();
+    if (eJsonData[channel]) {
+        epgData = eJsonData[channel];
+    } else {
+        channel = '未知频道';
+    }
+    // const epgData = eJsonData[channel][date];
+    const result = {
+        'date': date,
+        'channel_name': channel,
+        'epg_data': epgData
     };
-    return new Response(JSON.stringify(data), {
+    return new Response(JSON.stringify(result), {
         headers: {
             'Content-Type': 'application/json',
         },
