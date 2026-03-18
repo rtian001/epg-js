dccbfe7f5515ff8af11fc23500f3a2f1a1584de2
rtian001 <rtian001@gmail.com>
2026-03-18 12:23:29 +0800

    modified:   edge-functions/index.js

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -8,25 +8,10 @@ export default async function onRequest(context) {
   const currentDate = new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toISOString().slice(0, 10);
   const date = params.get('date') || currentDate;
   const host = req.headers.get('host');
-  var epgData = [{
-    start: '00:00',
-    end: '23:59',
-    title: '精彩节目'
-  }];
-
-  try {
-    const eJson = await fetch(`https://${host}/epg-${date}.json`);
-    const eJsonData = await eJson.json();
-    if (!eJsonData[channel]) {
-      channel = '未知频道';
-    } else {
-      epgData = eJsonData[channel];
-    }
-  } catch (error) {
-    console.log(JSON.stringify({ error: 'Internal Server Error' }));
-    channel = '未知频道';
-  }
-
+  const eJson = await fetch(`https://${host}/epg-${date}.json`);
+  const eJsonData = await eJson.json();
+  const epgData = eJsonData[channel] || [];
+  // const epgData = eJsonData[channel][date];
   const result = {
     'date': date,
     'channel_name': channel,
@@ -34,10 +19,7 @@ export default async function onRequest(context) {
   };
   return new Response(JSON.stringify(result), {
     headers: {
-      'Content-Type': 'application/json; charset=utf-8'
+      'Content-Type': 'application/json',
     },
   });
-
-
-  //default  
 }
