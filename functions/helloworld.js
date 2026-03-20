fde479f0a43becb73419dc5b9ccfebf301a4aacd
rtian <rtian1@163.com>
2026-03-20 19:25:43 +0800

    update

--- functions/helloworld.js
+++
@@ -1,9 +0,0 @@
-// 文件路径 ./edge-functions/api/hello.js
-// 访问路径 example.com/api/hello
-export default function onRequest(context) {
-  return new Response(JSON.stringify({ 'message': 'Hello from Epg!',success: true }), {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
