ede1534b11e7ec13838384f21de4200768049001
rtian001 <rtian001@gmail.com>
2026-03-20 17:07:33 +0800

    modified:   functions/index.js

--- functions/index.js
+++ functions/index.js
@@ -1,35 +1,9 @@
-const cookieName = "ab-test-cookie";
-const newHomepagePathName = "/test";
-
-const abTest = async (context) => {
-  const url = new URL(context.request.url);
-  // if homepage
-  if (url.pathname === "/") {
-    // if cookie ab-test-cookie=new then change the request to go to /test
-    // if no cookie set, pass x% of traffic and set a cookie value to "current" or "new"
-
-    let cookie = request.headers.get("cookie");
-    // is cookie set?
-    if (cookie && cookie.includes(`${cookieName}=new`)) {
-      // pass the request to /test
-      url.pathname = newHomepagePathName;
-      return context.env.ASSETS.fetch(url);
-    } else {
-      const percentage = Math.floor(Math.random() * 100);
-      let version = "current"; // default version
-      // change pathname and version name for 50% of traffic
-      if (percentage < 50) {
-        url.pathname = newHomepagePathName;
-        version = "new";
-      }
-      // get the static file from ASSETS, and attach a cookie
-      const asset = await context.env.ASSETS.fetch(url);
-      let response = new Response(asset.body, asset);
-      response.headers.append("Set-Cookie", `${cookieName}=${version}; path=/`);
-      return response;
-    }
-  }
-  return context.next();
-};
-
-export const onRequest = [abTest];
+// 文件路径 ./edge-functions/api/hello.js
+// 访问路径 example.com/api/hello
+export default function onRequest(context) {
+  return new Response(JSON.stringify({ 'message': 'Hello from Page Functions!', success: true }), {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
