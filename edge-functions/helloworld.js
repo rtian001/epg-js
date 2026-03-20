b54fa9d693d3baa49c5847a2e219da50858b4e72
rtian001 <rtian001@gmail.com>
2026-03-21 07:58:11 +0800

    Update helloworld.js

--- edge-functions/helloworld.js
+++ edge-functions/helloworld.js
@@ -1,6 +1,6 @@
 export default async function onRequest(context) {
   // 通过相对路径读取同目录下的JSON文件
-  const data = await import('/epg-2026-03-21.json', {
+  const data = await import('../epg-2026-03-21.json', {
     assert: { type: 'json' }
   });
   return new Response(JSON.stringify(data.default), {
