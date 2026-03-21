c5752ea032facb8cd7b0434a215ed817e8a76df4
rtian001 <rtian001@gmail.com>
2026-03-21 08:26:44 +0800

    Update index.js

--- edge-functions/index.js
+++ edge-functions/index.js
@@ -46,7 +46,7 @@ export default async function onRequest(context) {
     // const eJson = await fetch(`https://${host}/epg-${date}.json`);
     // if (eJson.ok) {
     //   const eJsonData = await eJson.json();
-    let eJsonData = await import(filename, {assert: { type: 'json' }});
+    let eJsonData = await import('../epg-2026-03-21.json', {assert: { type: 'json' }});
     if(!!eJsonData){
       if (_channel.startsWith('cctv')) {
         _channel = _channel.replace(/-/g, '').replace(/[^\x00-\xff]/g, '');
